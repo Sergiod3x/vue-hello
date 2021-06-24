a7424d4b23d6e2acebcb08d2b00d972265333660
Sergiod3x <sergiod3x.@gmail.com>
2021-06-24 16:09:34 +0200

    no bonus ok

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            // qui dentro metteremo tutti i dati
+            // della nostra applicazione.
+            // la nostra app si attiverà e verrà 
+            // eseguita in funzione di...
+            name: 'Beni benius in Sardinia, salludi a tottusu!',
+        },
+        // methods: {
+        //     saluta: function () {
+        //         // this è come un alias (per adesso) di data
+        //         alert(`Ciao ${this.name}!`);
+        //     },
+        //     showElement: function () {
+        //         if (this.contentClass === 'hidden') {
+        //             this.buttonText = 'Hide me!'
+        //             this.contentClass = 'show';
+        //         } else {
+        //             this.buttonText = 'Show me!'
+        //             this.contentClass = 'hidden';
+        //         }
+        //     }
+        // }
+    }
+ );
+  
+
